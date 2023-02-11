5f8cc22279b1a7f5a79e172e4975247cff3d5b1a
CantarinoG <guilhermecantarino1709@gmail.com>
2023-02-11 20:40:53 -0300

    Add logic

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
+const messages = [
+  {
+    text: "Wanna see nirvana but don't wanna die.",
+    user: "Cristopher Francis",
+    added: new Date().toISOString()
+  },
+  {
+    text: "All else fails, I was myself.",
+    user: "Mr. Duckworth",
+    added: new Date().toISOString()
+  }
+];
+
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Message Board', messages: messages });
+});
+
+router.get('/new', function(req, res, next) {
+  res.render('form');
+});
+
+router.post('/new', function (req, res, next) {
+  messages.push({
+    text: req.body.message,
+    user: req.body.user,
+    added: new Date().toISOString()
+  });
+  res.redirect('/');
 });
 
 module.exports = router;
