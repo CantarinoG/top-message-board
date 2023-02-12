54ee762aeb38a0d1e1c2b7a8687d8d33952aeb61
CantarinoG <guilhermecantarino1709@gmail.com>
2023-02-11 21:56:38 -0300

    Add basic styling

--- routes/index.js
+++ routes/index.js
@@ -16,7 +16,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Message Board', messages: messages });
+  res.render('index', { messages: messages });
 });
 
 router.get('/new', function(req, res, next) {
